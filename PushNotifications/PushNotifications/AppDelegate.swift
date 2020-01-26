ad5755e66fe48eebe454c84b5e57ca87a71b40f6
Ygor Nascimento <ygor.nascimento@outlook.com>
2020-01-26 18:15:13 -0300

    Notifications working on background

--- PushNotifications/PushNotifications/AppDelegate.swift
+++ PushNotifications/PushNotifications/AppDelegate.swift
@@ -7,31 +7,32 @@
 //
 
 import UIKit
+import UserNotifications
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder {
 
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
-    }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
+    var window: UIWindow?
 }
 
+extension AppDelegate: UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        //1. - Asking for user permission on app start
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (granted, _) in
+            guard granted else { return }
+            
+            //2. - If the permission was "granted", we register the app with the APN
+            DispatchQueue.main.async {
+                application.registerForRemoteNotifications()
+            }
+        }
+        return true
+    }
+    
+    //3. - Getting the device token from APN
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        let token = deviceToken.reduce("") {$0 + String(format: "%02x", $1)}
+        print("Device token: \(token)")
+    }
+}
